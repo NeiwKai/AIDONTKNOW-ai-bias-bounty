ea0f5a104494109d680cd4174e1c85ee44f580f1
Suppanat Timngarm <mr.sup@Suppanats-MacBook-Pro.local>
2025-07-01 11:57:23 +0700

    add feature to support 'Loan_Approved'

--- loan_model.py
+++ loan_model.py
@@ -11,105 +11,66 @@ from aif360.datasets import BinaryLabelDataset
 from aif360.metrics import BinaryLabelDatasetMetric
 
 data = pd.read_csv("datasets/loan_access_dataset.csv")
-binary_cols = ['Disability_Status', 'Criminal_Record']
+
 if 'ID' in data.columns:
     data = data.drop(columns=['ID'])
-data[binary_cols] = data[binary_cols].replace({'No': 0, 'Yes': 1})
+
 # Replace string values with integers
+binary_cols = ['Disability_Status', 'Criminal_Record']
+data[binary_cols] = data[binary_cols].replace({'No': 0, 'Yes': 1})
 data['Gender'] = data['Gender'].replace({'Male': 1, 'Female': 0, 'Non-binary': 2}).astype(int)
 data['Race'] = data['Race'].replace({'White': 0, 'Black': 1, 'Hispanic': 2, 'Multiracial': 3, 'Native American': 4, 'Asian': 5}).astype(int)
+data['Loan_Approved'] = data['Loan_Approved'].replace({'Approved': 1, 'Denied': 0}).astype(int)
+data['Employment_Type'] = data['Employment_Type'].replace({'Unemployed': 0, 'Part-time': 1, 'Gig': 2, 'Self-employed': 3, 'Full-time': 4}).astype(int)
+data['Education_Level'] = data['Education_Level'].replace({'High School': 0, 'Some College': 1, "Bachelor's": 2, 'Graduate': 3}).astype(int)
 
+# accept only numeric data
 data = data.select_dtypes(include=[np.number])
 
 print(data.columns)
 print(data.head())
 
-'''
-# encode the data that does not numeric
-categorical_cols = data.select_dtypes(include=['object', 'category']).columns
-data_encoded = pd.get_dummies(data, columns=categorical_cols, drop_first=True)
-data_encoded = data_encoded.astype(int)
+# define Privilege
+data['Gender_Male_priv'] = (data['Gender'] == 1).astype(int)
+data['Gender_Female_priv'] = (data['Gender'] == 0).astype(int)
+data['Gender_Non-binary_priv'] = (data['Gender'] == 2).astype(int) 
+data['Race_White_priv'] = (data['Race'] == 0).astype(int)
+data['Race_Black_priv'] = (data['Race'] == 1).astype(int)
+data['Race_Asian_priv'] = (data['Race'] == 5).astype(int)
+data['Race_Native-American_priv'] = (data['Race'] == 4).astype(int)
 
-print(data_encoded.info())
-#print(data_encoded.head())
-'''
-
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(data)
-
-# model initialize
-model = IsolationForest(contamination=0.05, random_state=42)
-model.fit(X_scaled)
-
-anomaly_labels = model.predict(X_scaled)
-
-data['Anomaly'] = anomaly_labels
-
-print(data['Anomaly'].value_counts())
-
-print(data[data['Anomaly'] == -1].head())  # Top anomalous samples
-
-num_non_criminal_anomalies = data[(data['Criminal_Record'] == 0) & (data['Anomaly'] == -1)].shape[0]
-print(f"Number of anomalous applicants without a criminal record: {num_non_criminal_anomalies}")
-
-num_non_criminal = data[(data['Criminal_Record'] == 0) & (data['Anomaly'] == 0)].shape[0]
-print(f"Number of non anomalous applicants without a criminal record: {num_non_criminal}")
-
-num_non_disability_anomalies = data[(data['Disability_Status'] == 0) & (data['Anomaly'] == -1)].shape[0]
-print(f"Number of anomalous applicants without disability: {num_non_disability_anomalies}")
-
-num_non_disability = data[(data['Disability_Status'] == 0) & (data['Anomaly'] == 0)].shape[0]
-print(f"Number of non anomalous applicants without disability: {num_non_disability}")
-
-'''
-# Visualization
-pca = PCA(n_components=2)
-X_pca = pca.fit_transform(X_scaled)
-
-plt.scatter(X_pca[:, 0], X_pca[:, 1], c=anomaly_labels, cmap='coolwarm', s=50)
-plt.title("Anomaly Detection with Isolation Forest")
-plt.xlabel("PC1")
-plt.ylabel("PC2")
-plt.show()
-'''
-'''
-df_aif = data.copy()
-
-protected_attribute_names = ['Gender', 'Race']
-label_name = 'Anomaly'
-
-aif_dataset = BinaryLabelDataset(
-    df=df_aif,
-    label_names=[label_name],
-    protected_attribute_names=protected_attribute_names
-)
-
-# Analyze fairness with respect to 'Gender'
-metric = BinaryLabelDatasetMetric(aif_dataset, privileged_groups=[{'Gender': 1}], unprivileged_groups=[{'Gender': 0}])
-
-print("Disparate Impact:", metric.disparate_impact())
-print("Statistical Parity Difference:", metric.statistical_parity_difference())
-'''
-
-data['Gender_Male_priv'] = (data['Gender'] == 1).astype(int)  # Male as privileged group
-data['Race_White_priv'] = (data['Race'] == 0).astype(int)  # White as privileged group
-
-aif_dataset = BinaryLabelDataset(
+# Bias against Black
+privileged_groups_black = [{'Race_Black_priv': 0}]
+unprivileged_grpup_black = [{'Race_Black_priv': 1}]
+alf_dataset_black = BinaryLabelDataset(
     df=data,
-    label_names=['Anomaly'],
-    protected_attribute_names=['Gender_Male_priv', 'Race_White_priv'],
+    label_names=['Loan_Approved'],
+    protected_attribute_names=['Race_Black_priv'],
     favorable_label=1,
-    unfavorable_label=-1  # Note: your anomaly labels are 1 and -1 from IsolationForest
+    unfavorable_label=0
 )
+metric_black = BinaryLabelDatasetMetric(alf_dataset_black,
+                                        privileged_groups=privileged_groups_black,
+                                        unprivileged_groups=unprivileged_grpup_black)
+print("Bias against Black")
+print("Disparate Impact", metric_black.disparate_impact())
+print("Statistical Parity Difference:", metric_black.statistical_parity_difference())
+print()
 
-aif_dataset.privileged_protected_attributes = np.array([[1]])
-
-privileged_groups = [{'Gender_Male_priv': 1, 'Race_White_priv': 0}]
-unprivileged_groups = [{'Gender_Male_priv': 0, 'Race_White_priv': 1}]
-
-metric = BinaryLabelDatasetMetric(aif_dataset, 
-                                  privileged_groups=privileged_groups, 
-                                  unprivileged_groups=unprivileged_groups)
-
-print("Disparate Impact:", metric.disparate_impact())
-print("Statistical Parity Difference:", metric.statistical_parity_difference())
+# Bias against Asian
+privileged_groups_asian = [{'Race_Asian_priv': 0}]
+unprivileged_grpup_asian = [{'Race_Asian_priv': 1}]
+alf_dataset_asian = BinaryLabelDataset(
+    df=data,
+    label_names=['Loan_Approved'],
+    protected_attribute_names=['Race_Asian_priv'],
+    favorable_label=1,
+    unfavorable_label=0
+)
+metric_asian = BinaryLabelDatasetMetric(alf_dataset_asian,
+                                        privileged_groups=privileged_groups_asian,
+                                        unprivileged_groups=unprivileged_grpup_asian)
+print("Bias against Asian")
+print("Disparate Impact", metric_asian.disparate_impact())
+print("Statistical Parity Difference:", metric_asian.statistical_parity_difference())
+print()
