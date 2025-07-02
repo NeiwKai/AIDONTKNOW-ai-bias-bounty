932a32547dd4800a1a59579c3a5411b2d3b996a0
NeiwKai <111691144+NeiwKai@users.noreply.github.com>
2025-07-02 21:04:48 +0700

    be able to predict

--- dataset_loader.py
+++ dataset_loader.py
@@ -14,31 +14,23 @@ def credit_score_level(score):
         return 0
 
 def dataset_loader(data):
-
-    if 'ID' in data.columns:
-        data = data.drop(columns=['ID'])
-    
-    data['Loan_Approved'] = data['Loan_Approved'].replace({'Approved': 1, 'Denied': 0}).astype(int)
+    if 'Loan_Approved' in data.columns:
+            data['Loan_Approved'] = data['Loan_Approved'].replace({'Approved': 1, 'Denied': 0}).astype(int)
     # Replace string values with integer
     binary_cols = ['Disability_Status', 'Criminal_Record']
     data[binary_cols] = data[binary_cols].replace({'No': 0, 'Yes': 1})
     
     #for bias_detection
-    '''
     data['Gender'] = data['Gender'].replace({'Male': 1, 'Female': 0, 'Non-binary': 2}).astype(int)
     data['Race'] = data['Race'].replace({'White': 0, 'Black': 1, 'Hispanic': 2, 'Multiracial': 3, 'Native American': 4, 'Asian': 5}).astype(int)
-    '''
     # -----------------
-    # CatBoost
-    # Try to simplify data
-    data['Education_Level'] = data['Education_Level'].replace({'High School': 'Undergraduate', 'Some College': 'Undergraduate', "Bachelor's": 'Graduate'})
-    
+
     '''
     data = pd.get_dummies(data, columns=['Gender', 'Race',
                                          'Employment_Type', 'Education_Level', 'Zip_Code_Group',
                                          'Citizenship_Status', 'Age_Group', 'Language_Proficiency'], drop_first=True)
     '''
-    '''
+
     data['Employment_Type'] = data['Employment_Type'].replace({'Unemployed': 0, 'Part-time': 1, 'Gig': 2, 'Self-employed': 3, 'Full-time': 4}).astype(int)
     data['Education_Level'] = data['Education_Level'].replace({'High School': 0, 'Some College': 1, "Bachelor's": 2, 'Graduate': 3}).astype(int)
     data['Zip_Code_Group'] = data['Zip_Code_Group'].replace({'Historically Redlined': 0, 'Rural': 1, 'Urban Professional': 2,
@@ -46,7 +38,6 @@ def dataset_loader(data):
     data['Citizenship_Status'] = data['Citizenship_Status'].replace({'Visa Holder': 0, 'Permanent Resident': 1, 'Citizen': 2}).astype(int)
     data['Age_Group'] = data['Age_Group'].replace({'Over 60': 0, '25-60': 1, 'Under 25': 2}).astype(int)
     data['Language_Proficiency'] = data['Language_Proficiency'].replace({'Limited': 0, 'Fluent': 1}).astype(int)
-    '''
 
     # For further training
     data['Loan_to_Income'] = data['Loan_Amount'] / (data['Income'] + 1)  # +1 to avoid division by zero
@@ -54,4 +45,3 @@ def dataset_loader(data):
     data['Credit_Level'] = data['Credit_Score'].apply(credit_score_level)
 
     return data
-
